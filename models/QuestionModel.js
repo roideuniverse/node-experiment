a9b546cbe0e80e71a30b17aa9785fa1e0c461aed
Kaushik Saurabh <roideuniverse@gmail.com>
2015-05-03 20:44:04 -0400

    Add QuestionModel and save it in the database

---
+++ models/QuestionModel.js
@@ -0,0 +1,17 @@
+/**  */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// create a schema
+var questionSchema = new Schema({
+    question: { type: String, required: true, unique: true },
+    tags: { type: String, required: true },
+    difficulty: { type: String, required: true }
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var QuestionModel = mongoose.model('Question', questionSchema);
+
+// make this available to our users in our Node applications
+module.exports = QuestionModel;
+/**  */
